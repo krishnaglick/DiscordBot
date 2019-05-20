a8ebab860de3e3ad2a6583dfb5e4f9914b116cf7
kaj103 <kjeannite@gmail.com>
2019-05-19 23:34:08 -0400

    May 19th, 2019
    - Created date.js
    - When you call date it returns a picture resulting the days of the week

---
+++ commands/date.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: 'date',
+	description: 'Sends message [its "insert day here" my dudes.]',
+	execute(message) {
+        // Gets current date, if date ==
+        let date = new Date().getDay()
+        console.log(date)
+        // 0 is Sunday, 1 is monday, and so on
+        if(date == 0)
+            message.channel.send("https://imgur.com/JcsIYJs")
+        if (date == 1)
+             message.channel.send("https://imgur.com/q1J8xO5")
+        if (date == 2)
+            message.channel.send("https://imgur.com/IDpAG8c")
+        if (date == 3)
+            message.channel.send("https://imgur.com/D8407BW")
+        if (date == 4)
+            message.channel.send("https://imgur.com/BMYUtr7")
+        if (date == 5)
+            message.channel.send("https://imgur.com/XDqrJMG")
+        if (date == 6)
+            message.channel.send("https://imgur.com/LapzGfR")
+	},
+};
