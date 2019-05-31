e79993cf814a4ff4f9c88635972f467841077bfe
Tacobell <chessegrater@gmail.com>
2019-05-31 00:32:13 -0400

    Add files via upload

--- commands/wf.js
+++ commands/wf.js
@@ -2,45 +2,39 @@ const cheerio = require('cheerio');
 const request = require('request');
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
-const cheerioTableparser = require('cheerio-tableparser')
+//const cheerioTableparser = require('cheerio-tableparser')
 
 //So the discord wrapper runs without problems which is nice. Nothing happens of course because I think it has to be ran on the server
-/*module.exports = {
-    name: 'debug',
-    description: 'Conduct an RPG battle',
-    execute(message, args) {*/
+module.exports = {
+    name: 'Warframe Weapons, maybe frames?',
+    description: 'Find weapons',
+    execute(message, args) {
 
-    const url = 'https://warframe.fandom.com/wiki/';
-
-function warframeSite(pendingHTML) {
-    return fetch(`${url}${pendingHTML}`)
-        .then(response => response.text());
-}
+    const url = 'https://warframe.fandom.com/wiki/'+ args[0];
 
 request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const gunName = $('h1').html()
+        const body = $('body')
+        const labelArr = {}
+        const valueArr = {}
+        const value = body.find($('.pi-data-value')).each(function (i, elem) {
+            valueArr[i] = $(this).text()
+        })
+        const label = body.find($('.pi-data-label')).each(function (i, elem) {
+            labelArr[i] = $(this).text()
+        })
+        console.log(labelArr[6]);
+        const embed = new Discord.RichEmbed()
+            .addField("Some bullshit",valueArr[1],true)
+            .addField("Another bullshit",labelArr[2],true)
+        message.channel.send({embed})
 
-
-        warframeSite('Fulmin')
-            .then(body => {
-                const $ = cheerio.load(body);
-                $('.portable-infobox').each(/*function */(i, element)=>  {
-                    const $element = $(element)
-                        //this was supposed to remove spaces, it kind of did
-                        .text()
-                        .replace(/\s\s+/);
-                    //character limit reached, shit's broken til I break it up. I should honestly use the table parser
-                    /*const embed = new Discord.RichEmbed()
-                        .setDescription($element)
-                    message.channel.send({embed});*/
-                    console.log($element);
-                });
-
-            });
     }
 })
-//},
-//};
+},
+};
 
 
 /*
