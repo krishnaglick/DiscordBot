756a014a52d1b566a68d2e305ba21231051d1b03
Tacobell <franklinlantigua90@gmail.com>
2019-06-01 17:38:59 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -12,7 +12,7 @@ module.exports = {
         function searchString (str, array1, array2) {
             for (var j=0; j<array1.length; j++) {
                 if (array1[j].match(str)) {
-                    return array[j], array[j];
+                    return array1[j], array2[j];
                 } else {
                     return -1;
                 }
