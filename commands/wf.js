3673c280b34f8f18386d3a57226489bde97b3ce5
Tacobell <franklinlantigua90@gmail.com>
2019-06-01 16:31:18 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -13,7 +13,7 @@ module.exports = {
             for (var j=0; j<array.length; j++) {
                 if (array[j].match(str)) return [labelArr[j]];
             }
-            return -1;
+            return labelArr[j];
         }
 
 
