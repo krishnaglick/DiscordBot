4336402cc2a06bf517afa0f510905698733cf723
Tacobell <franklinlantigua90@gmail.com>
2019-06-01 16:07:03 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -9,8 +9,14 @@ module.exports = {
     name: 'wf',
     description: 'Find weapons',
     execute(message, args) {
-
+        function searchString (str, array) {
+            for (var j=0; j<array.length; j++) {
+                if (array[j].match(str)) return [labelArr[j], valueArr[j]];
+            }
+            return -1;
+        }
     var url = 'https://warframe.fandom.com/wiki/'+ args[x];
+        var append = "";
         for(var x = 0; x < args.length; x++){
             if(x !== 0){
                 append += "_";
@@ -34,14 +40,15 @@ request(url, (error, response, html) => {
         const label = body.find($('.pi-data-label')).each(function (i, elem) {
             labelArr[i] = $(this).text()
         })
+        console.log(searchString('Disposition', 'labelArr[i]'));
         var image = imageLocation.find($('a.image.image-thumbnail')).attr('href');
 
         const embed = new Discord.RichEmbed()
             .setImage(image)
-            .addField("Label","MR"+"\n"+"Slot"+"\n"+"Type"+"\n"+labelArr[3]+"\n"+labelArr[4]+"\n"+labelArr[5]+"\n"+labelArr[6]+"\n"+labelArr[7]+"\n"+labelArr[8]+"\n"+labelArr[9]+"\n"+labelArr[10]+"\n"+labelArr[11]+"\n"+labelArr[12],true)
-            .addField("Value",valueArr[1]+"\n"+valueArr[2]+"\n"+valueArr[3]+"\n"+valueArr[4]+"\n"+valueArr[5]+"\n"+valueArr[6]+"\n"+valueArr[7]+"\n"+valueArr[8]+"\n"+valueArr[9]+"\n"+valueArr[10]+"\n"+valueArr[11]+"\n"+valueArr[12]+"\n",true)
+            //.addField("Label","MR"+"\n"+"Slot"+"\n"+"Type"+"\n"+labelArr[3]+"\n"+labelArr[4]+"\n"+labelArr[5]+"\n"+labelArr[6]+"\n"+labelArr[7]+"\n"+labelArr[8]+"\n"+labelArr[9]+"\n"+labelArr[10]+"\n"+labelArr[11]+"\n"+labelArr[12],true)
+            //.addField("Value",valueArr[1]+"\n"+valueArr[2]+"\n"+valueArr[3]+"\n"+valueArr[4]+"\n"+valueArr[5]+"\n"+valueArr[6]+"\n"+valueArr[7]+"\n"+valueArr[8]+"\n"+valueArr[9]+"\n"+valueArr[10]+"\n"+valueArr[11]+"\n"+valueArr[12]+"\n",true)
         message.channel.send({embed})
-        console.log(image);
+        //console.log(image);
 /*the first element (labelArr[0]) seems to be the image of the gun
 * */
 
@@ -49,21 +56,6 @@ request(url, (error, response, html) => {
 })
 },
 };
-
-
-/*
-=================***********=================****************=================***********=================
-=================***********=================Test Shit Ignore=================***********=================
-=================***********=================****************=================***********=================
-url = 'https://warframe.fandom.com/wiki/Boltor Prime'
-
-request(url, (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const body = $('body')
-
-    }
-}*/
 
 
 
