c102702786fee3f604cc7fe41dc84d1bd62c6f25
Tacobell <franklinlantigua90@gmail.com>
2019-06-01 16:28:00 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -25,7 +25,7 @@ module.exports = {
             append += args[x].charAt(0).toUpperCase() + args[x].slice(1);
         }
 
-        var url = 'https://warframe.fandom.com/wiki/'+ arg[x];
+        var url = 'https://warframe.fandom.com/wiki/'+ append.replace(" ","_");
 
 request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
@@ -43,11 +43,12 @@ request(url, (error, response, html) => {
         const label = body.find($('.pi-data-label')).each(function (i, elem) {
             labelArr[i] = $(this).text()
         })
-        console.log(searchString('Disposition', 'labelArr[i]'));
+        console.log(searchString('Reload Time', 'labelArr[i]'));
         var image = imageLocation.find($('a.image.image-thumbnail')).attr('href');
 
         const embed = new Discord.RichEmbed()
             .setImage(image)
+            .addField(searchString('Reload Time', 'labelArr[i]'))
             //.addField("Label","MR"+"\n"+"Slot"+"\n"+"Type"+"\n"+labelArr[3]+"\n"+labelArr[4]+"\n"+labelArr[5]+"\n"+labelArr[6]+"\n"+labelArr[7]+"\n"+labelArr[8]+"\n"+labelArr[9]+"\n"+labelArr[10]+"\n"+labelArr[11]+"\n"+labelArr[12],true)
             //.addField("Value",valueArr[1]+"\n"+valueArr[2]+"\n"+valueArr[3]+"\n"+valueArr[4]+"\n"+valueArr[5]+"\n"+valueArr[6]+"\n"+valueArr[7]+"\n"+valueArr[8]+"\n"+valueArr[9]+"\n"+valueArr[10]+"\n"+valueArr[11]+"\n"+valueArr[12]+"\n",true)
         message.channel.send({embed})
