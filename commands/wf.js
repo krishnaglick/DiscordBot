8bf94bd3ab2683a718262fef265581d219bdfcc6
Tacobell <franklinlantigua90@gmail.com>
2019-06-01 05:14:23 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -25,7 +25,7 @@ request(url, (error, response, html) => {
         const label = body.find($('.pi-data-label')).each(function (i, elem) {
             labelArr[i] = $(this).text()
         })
-        var image = body.find($('.lzyPlcHld.lzyTrns.lzyLoaded')).attr('src');
+        var image = body.find($('.image.image-thumbnail')).attr('src');
 
         const embed = new Discord.RichEmbed()
             .setImage(image)
