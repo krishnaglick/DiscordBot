7d814e1fbbcb23644b994b598066860fc3617830
Tacobell <franklinlantigua90@gmail.com>
2019-06-03 00:28:01 -0400

    Update wf.js

--- commands/wf.js
+++ commands/wf.js
@@ -63,7 +63,7 @@ request(url, (error, response, html) => {
 
         const embed = new Discord.RichEmbed()
             .setImage(image)
-            .addField(labelArr[searchString("Disposition", labelArr, valueArr)] + labelArr[11])
+            .addField(searchString("Disposition", labelArr, valueArr) + labelArr[11])
             //.addField("Label","MR"+"\n"+"Slot"+"\n"+"Type"+"\n"+labelArr[3]+"\n"+labelArr[4]+"\n"+labelArr[5]+"\n"+labelArr[6]+"\n"+labelArr[7]+"\n"+labelArr[8]+"\n"+labelArr[9]+"\n"+labelArr[10]+"\n"+labelArr[11]+"\n"+labelArr[12],true)
             //.addField("Value",valueArr[1]+"\n"+valueArr[2]+"\n"+valueArr[3]+"\n"+valueArr[4]+"\n"+valueArr[5]+"\n"+valueArr[6]+"\n"+valueArr[7]+"\n"+valueArr[8]+"\n"+valueArr[9]+"\n"+valueArr[10]+"\n"+valueArr[11]+"\n"+valueArr[12]+"\n",true)
         message.channel.send({embed})
